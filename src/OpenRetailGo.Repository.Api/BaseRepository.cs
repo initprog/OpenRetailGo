3aa2f751cee00473f55a155c2e7856e4a833af60
initprog <indrawan@nagwos.com>
2020-11-21 11:33:36 -0600

    Added abstract BaseRepository.cs

---
+++ src/OpenRetailGo.Repository.Api/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRetailGo.Repository.Api
+{
+    abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity: class
+    {
+        public abstract int Delete(TEntity obj);
+        public abstract IList<TEntity> GetAll();
+        public abstract int Save(TEntity obj);
+        public abstract int Update(TEntity obj);
+    }
+}
